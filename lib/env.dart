94da1370fbe966bda47d104b5ae0d3ef49a0f734
giulionyakunga <60649768+giulionyakunga@users.noreply.github.com>
2025-06-23 15:03:04 +0300

    Update env.dart

--- lib/env.dart
+++ lib/env.dart
@@ -3,8 +3,8 @@ String company_name = "TELabs";
 
 // String backend_url = 'http://192.168.78.31:8085/';
 // String backend_url = 'http://localhost:8085/';
-String backend_url = 'http://tiketimkononi.telabs.co.tz:8085/';
-String backend_url_with_fallback_ip = 'http://167.86.66.79:8085/';
+String backend_url = 'https://tiketimkononi.telabs.co.tz/';
+String backend_url_with_fallback_ip = 'https://167.86.66.79/';
 
 // String backend_ws_url = 'ws://192.168.78.31:8086';
 // String backend_ws_url = 'ws://localhost:8086';
